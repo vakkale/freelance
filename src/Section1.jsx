ad1f58b469edaa0fa75802b3e474300674f91eea
vakkale <volkanakkale@vt.edu>
2023-07-03 18:26:18 -0400

    some mobile styling

--- src/Section1.jsx
+++ src/Section1.jsx
@@ -1,7 +1,9 @@
 const Section1 = () => {
   return (
     <div className="lg-card card-1">
-      <div className="lg-1-text-1">My goal is to deliver you a website that is</div>
+      <div className="lg-1-text-1">
+        My goal is to deliver you a website that is
+      </div>
       <div className="lg-1-text-2">Beautiful</div>
       <div className="lg-1-text-2">Practical</div>
       <div className="lg-1-text-2">Unique</div>
